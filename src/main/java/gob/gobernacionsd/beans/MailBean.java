104b450727c17dbcc3b4c93678a4343001098bfc
JBG INC <juanf_24_6@hotmail.com>
2015-12-09 01:56:07 -0400

    Home progress

---
+++ src/main/java/gob/gobernacionsd/beans/MailBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gob.gobernacionsd.beans;
+
+import javax.inject.Named;
+
+/**
+ *
+ * @author JBG INC
+ */
+@Named
+
+public class MailBean {
+ 
+    
+    public MailBean(){
+    
+}
+    public void send(){
+        
+    }
+    
+}
