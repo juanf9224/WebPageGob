32aae1335f1464fb1167bc62e35275a2e8614485
JBG INC <juanf_24_6@hotmail.com>
2015-12-14 21:51:05 -0400

    Home progress

---
+++ src/main/java/gob/gobernacionsd/beans/FileDownloadBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gob.gobernacionsd.beans;
+
+import java.io.InputStream;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletContext;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+/**
+ *
+ * @author JBG INC
+ */
+@Named
+public class FileDownloadBean {
+    
+    private StreamedContent file;
+     
+    public FileDownloadBean() {        
+        InputStream stream = ((ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/resources/files/nomina.pdf");
+        file = new DefaultStreamedContent(stream, "application/pdf", "nomina-gobernacionsd.pdf");
+    }
+ 
+    public StreamedContent getFile() {
+        return file;
+    }
+}
