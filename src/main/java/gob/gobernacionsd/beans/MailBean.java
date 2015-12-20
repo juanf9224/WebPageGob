e4abdf747bce139f43e162e77092ee9b581397ed
JBG INC <juanf_24_6@hotmail.com>
2015-12-20 01:26:03 -0400

    Home progress

--- src/main/java/gob/gobernacionsd/beans/MailBean.java
+++ src/main/java/gob/gobernacionsd/beans/MailBean.java
@@ -66,18 +66,19 @@ public class MailBean implements Serializable {
             final String password = "Soylaclave";
 
             Properties props = new Properties();
-            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.auth", "false");
             props.put("mail.smtp.starttls.enable", "true");
             props.put("mail.smtp.host", "smtp.gmail.com");
             props.put("mail.smtp.port", "587");
 
-            Session session = Session.getInstance(props,
+            Session session = Session.getInstance(props);
+                    /*,
                     new javax.mail.Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
                     return new PasswordAuthentication(username, password);
                 }
-            });
+            });*/
 
             try {
 
