0e1f72e165841463a0ae384cc35d0cbfbde134fc
JBG INC <juanf_24_6@hotmail.com>
2015-12-20 00:04:26 -0400

    Home progress

--- src/main/java/gob/gobernacionsd/beans/MailBean.java
+++ src/main/java/gob/gobernacionsd/beans/MailBean.java
@@ -69,7 +69,7 @@ public class MailBean implements Serializable {
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.enable", "true");
             props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.port", 587);
+            props.put("mail.smtp.port", "587");
 
             Session session = Session.getInstance(props,
                     new javax.mail.Authenticator() {
